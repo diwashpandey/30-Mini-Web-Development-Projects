bd8e06f06eba86dee2cd01bd8d7c459af1338868
DIwash Pandey <diwash.pandey.only@gmail.com>
2024-01-02 20:10:46 +0545

    added Music App (Project 8)
    In this commit I've added Project 8 which is a Music app.
    
    and made a minor change in Weather app.

---
+++ Project 8_Music Player/script.js
@@ -0,0 +1,67 @@
+const previousBtn = document.getElementById("previous")
+const playBtn = document.getElementById("play")
+const nextBtn = document.getElementById("next")
+const progress = document.getElementById("progressbar")
+const music = document.getElementById("music")
+const titlePhoto = document.getElementById("title-photo")
+
+
+
+music.onloadedmetadata = ()=>{
+    // progress.max = Math.floor(music.duration);
+    progress.max = 184;
+    progress.min = music.currentTime;
+
+}
+
+function showError(){
+    let errorbox = document.createElement("div");
+    errorbox.classList.add("toast");
+    errorbox.innerHTML= "Sorry, this button is not functionable";
+    document.querySelector(".app").appendChild(errorbox)
+    setTimeout(()=>{
+        errorbox.remove();
+    },2000)
+}
+
+function playPause(){
+    if(playBtn.classList.contains("paused")){
+        music.play()
+        playBtn.classList.remove("paused");
+        playBtn.classList.add("playing");
+        playBtn.src ="./Equipments/icons/pause.svg";
+        titlePhoto.classList.toggle("rotating")
+    }
+
+    else if (playBtn.classList.contains("playing")){
+        music.pause()
+
+        playBtn.classList.remove("playing");
+        playBtn.classList.add("paused");
+        
+        playBtn.src ="./Equipments/icons/play.svg";
+        titlePhoto.classList.toggle("rotating")
+    }
+}
+
+progress.onchange=()=>{
+    music.currentTime = progress.value;
+    music.play();
+
+
+    playBtn.src ="./Equipments/icons/pause.svg";
+        if(playBtn.classList.contains("paused")){playBtn.classList.remove("paused");
+        playBtn.classList.add("playing");
+        playBtn.src ="./Equipments/icons/pause.svg";
+        titlePhoto.classList.toggle("rotating")
+        }
+
+}
+
+if (music.play){
+    setInterval(()=>{
+        progress.value = music.currentTime
+    },1000)
+}
+
+
