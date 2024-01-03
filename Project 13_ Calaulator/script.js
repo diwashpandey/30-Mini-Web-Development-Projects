6d57b117489faef0d89ec4a8ef4991b79ffba5a0
DIwash Pandey <diwash.pandey.only@gmail.com>
2024-01-03 17:36:40 +0545

    Added Calculator (Project 13)
    Added Calaulator:
    -Fully functional
    -Works with Keyboard too

---
+++ Project 13_ Calaulator/script.js
@@ -0,0 +1,61 @@
+const display = document.getElementById("display")
+var values = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "+", "-", "/", ".", "Enter","Backspace","Escape", "Delete"]
+
+function addOnDisplay(data){
+    display.value = display.value+data;
+
+}
+
+function calculate(){
+    try{
+        display.value=eval(display.value)
+    }
+    catch(error){
+        display.value=""
+        display.placeholder="Error"
+    }
+}
+
+function del(){
+    display.value = display.value.toString().slice(0,-1)
+    console.log(display.value)
+}
+
+function check(){
+    if(display.value.length>9){
+        return false;
+    }
+    else{
+        return true;
+    }
+}  
+
+
+
+document.body.addEventListener("keydown",(k)=>{
+    display.placeholder=""
+    let which = k.key.toString();
+    if (values.includes(which)){
+        if (which=="Enter"){
+            calculate()
+        }
+        else if(which=="Backspace"){
+            del()
+        }
+        else if(which=="Escape" || which=="Delete"){
+            display.value = ''
+        }
+        else{
+            addOnDisplay(which)
+        }
+        
+        
+    }
+    else{
+        console.log("inside else")
+    }
+})
+
+document.body.addEventListener("mousedown",()=>{
+    display.placeholder=""
+})
