97ba4f1dd8c71c6d734ab7ed46de048425b64ae3
DIwash Pandey <diwash.pandey.only@gmail.com>
2024-01-03 15:18:17 +0545

    Theme Mode Website Added
    Woahhh, Learned new thing with this

---
+++ Project 12_Theme Modes/script.js
@@ -0,0 +1,53 @@
+const sunIcon = document.getElementById("sun-icon")
+const moonIcon = document.getElementById("moon-icon")
+
+const body = document.body
+
+const logo = document.getElementById("logo")
+const hireBtn = document.querySelector(".content-box>button");
+
+
+function changeIcon(turn) {
+    if (turn == "dark") {
+        sunIcon.classList.remove("active-theme")
+        setTimeout(() => {
+            moonIcon.classList.add("active-theme")
+        }, 500)
+
+    }
+    else if (turn == "light") {
+        moonIcon.classList.remove("active-theme")
+        setTimeout(() => {
+            sunIcon.classList.add("active-theme")
+        }, 500)
+    }
+    else {
+        alert("Sorry, Some Error Occured")
+    }
+}
+
+function differentForLogoAndButton(which) {
+    if (which == "dark") {
+        logo.style.fill = "white";
+        hireBtn.style.color = "white";
+
+    }
+    else if (which == "light") {
+        logo.style.fill = "var(--theme-background)";
+        hireBtn.style.fill = "var(--theme-background)";
+
+    }
+}
+
+function changeTheme() {
+    body.classList.toggle("dark_theme")
+
+    if (body.classList.contains("dark_theme")) {
+        differentForLogoAndButton("dark")
+        changeIcon("dark")
+    }
+    else {
+        differentForLogoAndButton("light")
+        changeIcon("light")
+    }
+}
